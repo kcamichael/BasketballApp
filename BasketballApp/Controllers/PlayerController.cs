f6141682c6316385feaa39997ed70b6d7f2c8351
Kevin Michael <kcamichael@gmail.com>
2023-09-13 21:15:11 -0400

    PlayerController Done!

--- BasketballApp/Controllers/PlayerController.cs
+++ BasketballApp/Controllers/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using BasketballApp.Service.PlayerServices;
+﻿using BasketballApp.Models.PlayerModels;
+using BasketballApp.Service.PlayerServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasketballApp.Controllers
@@ -12,12 +13,14 @@ namespace BasketballApp.Controllers
             _playerService = playerService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var players = await _playerService.GetPlayers();
             return View(players);
         }
 
+        [HttpGet]
         public async Task<IActionResult> Detail(int id)
         {
             var player = await _playerService.GetPlayer(id);
@@ -28,9 +31,73 @@ namespace BasketballApp.Controllers
             }
         }
 
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PlayerCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                var isSuccessful = await _playerService.AddPlayer(model);
+                if(isSuccessful)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(ModelState);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var player = await _playerService.GetPlayer(id);
+            if (player is null) return NotFound();
+            else
+            {
+                return View(player);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PlayerEdit model)
+        {
+            if (ModelState.IsValid)
+            {
+                var isSuccessful = await _playerService.UpdatePlayer(model);
+                if (isSuccessful)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(ModelState);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int/*?*/ ID)
+        {
+            var player = await _playerService.GetPlayer(ID);
+            if (player is null) return NotFound();
+            else
+                return View(player);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeletePlayer(int ID)
+        {
+            var player = await _playerService.GetPlayer(ID);
+            if (player is null) return NotFound();
+            else
+                await _playerService.DeletePlayer(ID);
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
